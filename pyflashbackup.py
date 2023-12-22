58385832253d339576060766e4d550a2f8c1d1a6
Stanislav Chubar <stas-chubar@yandex.ru>
2023-12-22 23:12:13 +0700

    pyflashbackup.py :: add logging and work with config presets

--- pyflashbackup.py
+++ pyflashbackup.py
@@ -1,13 +1,10 @@
-# Copyright 2023 Stanislav Chubar (as known as schubar or schubarkrsk)
-# This code licensed under Apache 2.0
-# Code provide "AS IS" without any warranty
-
-
 import os
 import shutil
 import time
 import psutil
 import pathlib
+import logging
+from configparser import ConfigParser
 
 USER_HOME = pathlib.Path.home()
 APP_HOME = pathlib.Path(USER_HOME, "flashbackup")
@@ -15,13 +12,54 @@ APP_HOME = pathlib.Path(USER_HOME, "flashbackup")
 if not os.path.exists(APP_HOME):
     os.makedirs(APP_HOME)
 
+CONFIG_FILE = os.path.join(APP_HOME, "pfb_config.ini")
+
+# Если конфигурационный файл не существует, создаем его с настройками по умолчанию
+if not os.path.exists(CONFIG_FILE):
+    config = ConfigParser()
+    config.optionxform = str  # отключаем преобразование ключей в нижний регистр
+    config["BackupConfig"] = {
+        "source_path": "",
+        "destination_path": str(APP_HOME),
+        "exclude_files": "*.tmp, *.bak",
+        "exclude_directories": "temp, cache, System Volume Information"
+    }
+
+    with open(CONFIG_FILE, "w") as config_file:
+        config.write(config_file)
+
+# Загружаем конфигурацию
+config = ConfigParser()
+config.optionxform = str  # отключаем преобразование ключей в нижний регистр
+config.read(CONFIG_FILE)
+
+# Получаем значения из конфигурации
+source_path = config.get("BackupConfig", "source_path")
+destination_path = config.get("BackupConfig", "destination_path")
+exclude_files = [pattern.strip() for pattern in config.get("BackupConfig", "exclude_files").split(",")]
+exclude_directories = [dir.strip() for dir in config.get("BackupConfig", "exclude_directories").split(",")]
+
+# Настройка логирования
+logging.basicConfig(filename=os.path.join(APP_HOME, "backup_log.txt"), level=logging.INFO)
+
+# Строка формата времени для создания уникальных имен папок
+backup_format = "Backup_%Y%m%d%H%M%S"
+
 
 def backup_flash_drive(source_path, destination_path):
     try:
-        shutil.copytree(source_path, destination_path)
-        print(f"Backup successfully created! Dest: {destination_path}")
+        timestamp = time.strftime(backup_format)
+        backup_path = os.path.join(destination_path, timestamp)
+
+        # Добавим игнорирование файлов и директорий
+        ignore = shutil.ignore_patterns(*exclude_files, *exclude_directories)
+
+        shutil.copytree(source_path, backup_path, ignore=ignore)
+        logging.info(f"Backup successfully created! Source: {source_path}, Dest: {backup_path}")
+    except shutil.Error as e:
+        logging.error(f"Backup failed: {e}")
     except Exception as e:
-        print(f"ERROR | Oh no, we have a error...: {e}")
+        logging.exception(f"An unexpected error occurred: {e}")
 
 
 def detect_flash_drive():
@@ -34,20 +72,17 @@ def detect_flash_drive():
 
 
 def main():
-    backup_folder = APP_HOME
+    backup_folder = str(APP_HOME)
 
     while True:
         flash_drive = detect_flash_drive()
 
         if flash_drive:
-            timestamp = time.strftime("%Y%m%d%H%M%S")
-            backup_path = os.path.join(backup_folder, f"Backup_{timestamp}")
-
-            backup_flash_drive(flash_drive, backup_path)
+            backup_flash_drive(flash_drive, destination_path)
 
             while detect_flash_drive() == flash_drive:
                 time.sleep(1)
-                print("Waiting for USB device...")
+                logging.info("Waiting for USB device...")
 
 
 if __name__ == "__main__":
